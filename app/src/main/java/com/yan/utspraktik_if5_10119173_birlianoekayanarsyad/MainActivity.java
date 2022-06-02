595a45c97036b8c900c0c8a3dd2c6310653ee06e
villian919 <villian919@gmail.com>
2022-06-02 19:39:59 +0700

    Initial commit

---
+++ app/src/main/java/com/yan/utspraktik_if5_10119173_birlianoekayanarsyad/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yan.utspraktik_if5_10119173_birlianoekayanarsyad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
